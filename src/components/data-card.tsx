f50526681a9996a273152e8d4d59b36e65cd2ae6
nerbrain <malikmox@gmail.com>
2026-07-05 20:23:41 +0300

    improved dashboard page with better layout

---
+++ src/components/data-card.tsx
@@ -0,0 +1,11 @@
+const DataCard = () => {
+    return (
+        <div className='block bg-white space-x-1.5 p-6 rounded-2xl space-y-2 min-w-48 shadow-lg'>
+            <p className='text-[#71717B] text-sm'>Active Links</p>
+            <p className='text-3xl font-light'>30</p>
+            <p className='text-[#71717B] text-sm'>out of total 35</p>
+        </div>
+    )
+}
+
+export default DataCard
